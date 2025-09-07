83c504e093466cd010a73896897736c47a78621e
vupwoz <vupwoz@gmail.com>
2025-09-06 19:56:01 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
+import App from "./App.jsx"; // add .jsx extension
+import "./style.css";         // import your CSS
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
